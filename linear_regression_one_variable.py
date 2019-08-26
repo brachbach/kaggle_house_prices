302c32bbfe07e1e42777518171ec95ddc0713862
Ben Rachbach <brachbach@gmail.com>
2019-08-26 13:01:45 -0700

    Predict using linear regression on LotArea alone

--- linear_regression_one_variable.py
+++ linear_regression_one_variable.py
@@ -1,15 +1,20 @@
-# predict test based on a simple linear regression
+# predict test based on a simple linear regression on train
 
 import pandas as pd
 
 from sklearn.linear_model import LinearRegression
 
 train = pd.read_csv("data_from_kaggle/train.csv")
-
-
-
 test = pd.read_csv("data_from_kaggle/test.csv")
+
+linearRegressor = LinearRegression()
+
+linearRegressor.fit(train.loc[ : , "LotArea"].values.reshape(-1,1), train.loc[ : , "SalePrice"])
+
+price_predictions = linearRegressor.predict(test.loc[ : , "LotArea"].values.reshape(-1,1))
+
+test["SalePrice"] = price_predictions
 
 # test["SalePrice"] = median
 
-# test.to_csv(path_or_buf="predictions/median.csv", columns=["Id", "SalePrice"], index=False)
+test.to_csv(path_or_buf="predictions/linear_regression_one_variable.csv", columns=["Id", "SalePrice"], index=False)
--- mean_value.py
+++
@@ -1,9 +0,0 @@
-# just predict that ever house in test will sell for the mean value from train
-
-import csv
-
-with open('data_from_kaggle/train.csv', 'r') as file:
-    reader = csv.reader(file)
-    as_list = list(reader)
-
-print(as_list)
